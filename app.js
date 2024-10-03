761bf54b89eef6038239a1f3898a280dd27fc537
Ljwadd1 <w0488136@nscc.ca>
2024-10-03 14:19:07 -0300

    Added CORS to app

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import contactsRouter from './routes/contacts.js';
+import cors from 'cors';
 
 // Setup
 const port = process.env.PORT || 3000;
 const app = express();
+//var cors = require('cors');
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(cors());
 
 // API routes
 app.use('/api/contacts', contactsRouter);
